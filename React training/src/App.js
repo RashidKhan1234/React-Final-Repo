569844f374ce75949dca0a011b42e93fa87db681
RashidKhan1234 <71687734+RashidKhan1234@users.noreply.github.com>
2020-09-22 15:50:47 +0530

    Code changes to understand State

--- React training/src/App.js
+++ React training/src/App.js
@@ -3,14 +3,20 @@ import "./styles.css";
 import Greet from "./Component/Greeting";
 import Welcome from "./Component/Welcome";
 import Hello from "./Component/HelloWithJSX";
+import HelloProps from "./Component/Props";
+import Message from "./Component/MessageWithState";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Greet />
-        <Welcome />
+        <Welcome name="Rashid Props Class" />
         <Hello />
+        <HelloProps name="Rashid Props">
+          <p>Rashid Paragraph</p>
+        </HelloProps>
+        <Message />
       </div>
     );
   }
